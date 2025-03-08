dd9be03ce6ec6a60d5751003b89048f73e730a22
KompanionSL <admin@kompanion.es>
2025-03-08 18:15:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Instalando Chromium y ChromeDriver..."
-apt-get update && apt-get install -y chromium-browser chromium-driver
+echo "Instalando Chromium compatible..."
+apt-get update && apt-get install -y chromium chromium-driver libglib2.0-0
 
 echo "Ejecutando la aplicación..."
-gunicorn bot_boe_selenium:app --timeout 120 --bind 0.0.0.0:$PORT
+exec gunicorn bot_boe:app --timeout 120 --bind 0.0.0.0:$PORT
